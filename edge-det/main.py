d79307d74110439ddc8b675d21d9f2714cf67d04
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-07-16 22:20:20 -0300

    adicionei uma analise das linhas e colunas da imagem apos o filtro OTSU

--- edge-det/main.py
+++ edge-det/main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 class iris_detection():
     def __init__(self, image_path):
         '''
@@ -25,52 +26,78 @@ class iris_detection():
     def convert_to_gray_scale(self):
         self._img = cv2.cvtColor(self._img, cv2.COLOR_BGR2GRAY)
 
-    def detect_pupil(self):
-        '''
-        This method should use cv2.findContours and cv2.HoughCircles() function from cv2 library to find the pupil
-        and then set the coordinates for pupil circle coordinates
-        '''
-        # First binarize the image so that findContours can work correctly.
-        _, thresh = cv2.threshold(self._img.copy(), 100, 255, cv2.THRESH_BINARY_INV)
-        cv2.imshow("Result tresh", thresh)
-        # Now find the contours and then find the pupil in the contours.
-        contours, hi = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        #c = cv2.HoughCircles(contours, cv2.HOUGH_GRADIENT, 2, self._img.shape[0]/2)
-        # Then mask the pupil from the image and store it's coordinates.
-        print(np.array(hi))
-        for cont in contours:
-            for c in cont:
-                #print(c[0])
-                x,y = c[0]
-                cv2.putText(self._img,'.',(x,y),cv2.FONT_HERSHEY_PLAIN,0.8,(255, 255, 255),1)
-        '''for l in c:
-            # OpenCV returns the circles as a list of lists of circles
-            for circle in l:
-                center = (circle[0], circle[1])
-                radius = circle[2]
-                cv2.circle(self._img, center, radius, (0, 0, 0), thickness = -1)
-                self._pupil = (center[0], center[1], radius)'''
-        
     def detect_iris(self):
-        '''
-        This method should use the background subtraction technique to isolate the iris from the original image
-        It should use the coordinates from the detect_pupil to get a larger circle using cv2.HoughCircles()
-        '''
-        _, t = cv2.threshold(self._img, 195, 255, cv2.THRESH_BINARY)
-        contours, _, _ = cv2.findContours(t, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        # Find the iris using the radius of the pupil as input.
-        c = cv2.HoughCircles(contours, cv2.HOUGH_GRADIENT, 2, self._pupil[2] * 2, param2 = 150)
+        
+        gray = cv2.cvtColor(self._img, cv2.COLOR_BGR2GRAY)
 
-        for l in c:
-            for circle in l:
-                center = (self._pupil[0], self._pupil[1])
-                radius = circle[2]
-                # This creates a black image and draws an iris-sized white circle in it.
-                mask = numpy.zeros((self._img.shape[0], self._img.shape[1], 1), numpy.uint8)
-                cv2.circle(mask, center, radius, (255, 255, 255), thickness = -1)
-                # Mask the iris and crop everything outside of its radius.
-                self._img = cv2.bitwise_and(self._img, mask)
+        ret, o1 = cv2.threshold(gray,0,255, cv2.THRESH_BINARY + cv2.THRESH_OTSU )
+        ret, o2 = cv2.threshold(gray,0,255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU )
+        ret, o3 = cv2.threshold(gray,0,255, cv2.THRESH_TOZERO + cv2.THRESH_OTSU )
+        ret, o4 = cv2.threshold(gray,0,255, cv2.THRESH_TOZERO_INV + cv2.THRESH_OTSU )
+        ret, o5 = cv2.threshold(gray,0,255, cv2.THRESH_TRUNC + cv2.THRESH_OTSU )
+        
+        '''cv2.imshow("OTSU 1", o1)
+        cv2.imshow("OTSU 2", o2)
+        cv2.imshow("OTSU 3", o3)
+        cv2.imshow("OTSU 4", o4)
+        cv2.imshow("OTSU 5", o5)'''
+        
+        blurred = cv2.bilateralFilter(o5,10,50,50)
+        print(blurred.shape)
+        
 
+
+
+        col_count = np.zeros((290))
+        row_count = []
+        for i in range(blurred.shape[0]):
+            aux_row = 0
+            aux_col = 0
+            for j in range(blurred.shape[1]):
+                aux_row = aux_row + blurred[i][j]/blurred.shape[1]
+                col_count[j] = col_count[j] + blurred[i][j]/blurred.shape[0]
+            row_count.append(aux_row)
+        cv2.imshow("Blurred", blurred)
+        print((col_count.shape))
+        print(np.array(row_count).shape)
+
+
+        
+        x_col = np.arange(0.0, blurred.shape[1], 1)
+        print(x_col.shape)
+        fig_c, col_ax = plt.subplots()
+        col_ax.plot(x_col, col_count)
+        col_ax.set(xlabel='Columns (s)', ylabel='Color Med',
+            title='Column Color count')
+        col_ax.grid()
+
+
+        x_row = np.arange(0.0, blurred.shape[0], 1)
+        fig_r, row_ax = plt.subplots()
+        row_ax.plot(x_row, row_count)
+        row_ax.set(xlabel='Rows (s)', ylabel='Color Med',
+            title='Row Color count')
+        row_ax.grid()
+        plt.show()
+
+
+        
+        plt.show()
+
+        minDist = 1
+        param1 = 20 # 500
+        param2 = 50 # 200 #smaller value-> more false circles
+        minRadius = 1
+        maxRadius = 300 #10
+
+        # docstring of HoughCircles: HoughCircles(image, method, dp, minDist[, circles[, param1[, param2[, minRadius[, maxRadius]]]]]) -> circles
+        circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, minDist, param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
+
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            for i in circles[0,:]:
+                cv2.circle(self._img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        print("HERE")
     def start_detection(self):
         '''
         This is the main method that will be called to detect the iris
@@ -83,9 +110,8 @@ class iris_detection():
         using the method cv2.imshow
         '''
         if(self.load_image()):
-            self.convert_to_gray_scale()
-            self.detect_pupil()
-            #self.detect_iris()
+            #self.convert_to_gray_scale()
+            self.detect_iris()
             cv2.imshow("Result", self._img)
             cv2.waitKey(0)
         else:
