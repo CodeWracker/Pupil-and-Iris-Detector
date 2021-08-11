bcddaadb42e2251a838a183ac579510ae16b4da7
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-08-11 15:35:13 -0300

    adicionei alguns filtros

--- edge-det/main.py
+++ edge-det/main.py
@@ -7,6 +7,7 @@ class iris_detection():
         initialize the class and set the class attributes
         '''
         self._img = None
+        self._img_out = None
         self._img_path = image_path
         self._pupil = None
 
@@ -17,6 +18,7 @@ class iris_detection():
         it should also detect if the file exists
         '''
         self._img = cv2.imread(self._img_path)
+        self._img_out = self._img
         # If the image doesn't exists or is not valid then imread returns None
         if type(self._img) is type(None):
             return False
@@ -29,26 +31,10 @@ class iris_detection():
     def detect_iris(self):
         
         gray = cv2.cvtColor(self._img, cv2.COLOR_BGR2GRAY)
-
-        ret, o1 = cv2.threshold(gray,0,255, cv2.THRESH_BINARY + cv2.THRESH_OTSU )
-        ret, o2 = cv2.threshold(gray,0,255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU )
-        ret, o3 = cv2.threshold(gray,0,255, cv2.THRESH_TOZERO + cv2.THRESH_OTSU )
-        ret, o4 = cv2.threshold(gray,0,255, cv2.THRESH_TOZERO_INV + cv2.THRESH_OTSU )
         ret, o5 = cv2.threshold(gray,0,255, cv2.THRESH_TRUNC + cv2.THRESH_OTSU )
-        
-        '''cv2.imshow("OTSU 1", o1)
-        cv2.imshow("OTSU 2", o2)
-        cv2.imshow("OTSU 3", o3)
-        cv2.imshow("OTSU 4", o4)
-        cv2.imshow("OTSU 5", o5)'''
-        
         blurred = cv2.bilateralFilter(o5,10,50,50)
         print(blurred.shape)
-        
         ret, bin_img = cv2.threshold(blurred,127,255,cv2.THRESH_BINARY)
-        cv2.imshow("Binario",bin_img)
-
-
         col_count = np.zeros((bin_img.shape[1]))
         row_count = []
         for i in range(bin_img.shape[0]):
@@ -58,30 +44,12 @@ class iris_detection():
                 aux_row = aux_row + bin_img[i][j]/bin_img.shape[1]
                 col_count[j] = col_count[j] + bin_img[i][j]/bin_img.shape[0]
             row_count.append(aux_row)
-        cv2.imshow("bin_img", bin_img)
         for i in range(len(row_count)):
             row_count[i] =( 255 -row_count[i])
         for i in range(len(col_count)):
             col_count[i] = (255 -col_count[i])
         
         row_count = np.array(row_count)
-        
-        x_col = np.arange(0.0, bin_img.shape[1], 1)
-        print(x_col.shape)
-        fig_c, col_ax = plt.subplots()
-        col_ax.plot(x_col, col_count)
-        col_ax.set(xlabel='Columns (s)', ylabel='Color Med',
-            title='Column Color count')
-        col_ax.grid()
-
-
-        x_row = np.arange(0.0, bin_img.shape[0], 1)
-        fig_r, row_ax = plt.subplots()
-        row_ax.plot(x_row, row_count)
-        row_ax.set(xlabel='Rows (s)', ylabel='Color Med',
-            title='Row Color count')
-        row_ax.grid()
-        plt.show()
         row_count = row_count - row_count.min()
         media_r = 0
         for i,row in enumerate(row_count):
@@ -106,24 +74,54 @@ class iris_detection():
         print("raio: ",desvio_c,desvio_r)
 
         #cv2.putText(self._img,'+',(int(media_c),int(media_r)),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),1)
-        cv2.ellipse(self._img, (int(media_c),int(media_r)), (int(desvio_c),int(desvio_r)),0,0,360, (0, 0, 255), 2)
+        cv2.ellipse(self._img_out, (int(media_c),int(media_r)), (int(desvio_c),int(desvio_c)),0,0,360, (0, 0, 255), 2)
 
 
-        minDist = 1
-        param1 = 20 # 500
-        param2 = 50 # 200 #smaller value-> more false circles
-        minRadius = 1
-        maxRadius = 300 #10
+    def test(self):
+        self.convert_to_gray_scale()
+        new_img = np.zeros(self._img.shape)
+        min_val = self._img.min() + 20
+        print(new_img.shape)
+        print(min_val)
+        for i in range(0,new_img.shape[0]):
+            for j in range(0,new_img.shape[1]):
+                if(self._img[i][j]>min_val):
+                    new_img[i][j] = 255 
+        cv2.imshow("test",new_img)
+        cv2.waitKey(0)
+    def detect_pupil(self):
+        # Read image
+        
+        self.convert_to_gray_scale()
+        
 
-        # docstring of HoughCircles: HoughCircles(image, method, dp, minDist[, circles[, param1[, param2[, minRadius[, maxRadius]]]]]) -> circles
-        circles = cv2.HoughCircles(bin_img, cv2.HOUGH_GRADIENT, 1, minDist, param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
+        new_img = np.zeros(self._img.shape, dtype=self._img.dtype)
+        min_val = self._img.min() + 40
+        print(new_img.shape)
+        print(min_val)
+        for i in range(0,new_img.shape[0]):
+            for j in range(0,new_img.shape[1]):
+                if(self._img[i][j]>min_val):
+                    new_img[i][j] = 255 
+        new_img = np.array(new_img)
+        
+        kernel = np.ones((5,5),np.uint8)
+        cv2.imshow("d",new_img)
+        new_img = cv2.morphologyEx(new_img, cv2.MORPH_OPEN, kernel)
+        cv2.imshow("MORPH_OPEN",new_img)
+        new_img = cv2.morphologyEx(new_img, cv2.MORPH_CLOSE, kernel)
+        cv2.imshow("MORPH_CLOSE",new_img)
+        cv2.waitKey(0)
+        contours,hierarchy = cv2.findContours(new_img, 1, 2)
+        print(len(contours))
+        cnt = contours[0]
+        (x,y),radius = cv2.minEnclosingCircle(cnt)
+        print(x,y)
+        center = (int(x),int(y))
+        radius = int(radius)
+        cv2.circle(self._img_out,center,radius,(255,255,255),2)
+        
 
-        if circles is not None:
-            circles = np.uint16(np.around(circles))
-            for i in circles[0,:]:
-                print(i[0], i[1], i[2])
-                cv2.circle(self._img, (i[0], i[1]), i[2], (0, 255, 0), 2)
-        print("HERE")
     def start_detection(self):
         '''
         This is the main method that will be called to detect the iris
@@ -137,8 +135,9 @@ class iris_detection():
         '''
         if(self.load_image()):
             #self.convert_to_gray_scale()
-            self.detect_iris()
-            cv2.imshow("Result", self._img)
+            #self.detect_iris()
+            self.detect_pupil()
+            cv2.imshow("Result", self._img_out)
             cv2.waitKey(0)
         else:
             print('Image file "' + self._img_path + '" could not be loaded.')
